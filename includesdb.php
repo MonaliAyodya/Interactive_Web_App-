60b75b174e53857202968c1a798cff68b54509fd
Monali Dissanayake <dissanayakemonali72@gmail.com>
2025-04-24 18:10:59 +0530

    updte includesbd
    updte includesbd

--- includesdb.php
+++ includesdb.php
@@ -4,7 +4,7 @@ $user = "root";
 $password = "";
 $database = "recipe_book";
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $recipe_book);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
